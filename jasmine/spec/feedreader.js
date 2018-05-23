81140406640e72a64655aee224092ec016185b74
Robert-Roy <robertproy@live.com>
2018-05-23 17:28:09 -0500

    implement testing for the RSS feed definitions

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,19 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        it('should have valid URLs', function(){
+            for(var i = 0, len = allFeeds.length; i<len;i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+        })
+        
+        it('should have valid names', function(){
+            for(var i = 0, len = allFeeds.length; i<len;i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+        })
     });
 
 
