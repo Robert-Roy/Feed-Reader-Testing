f23fbae08ec516951823b988bb62a8336d76025a
Robert-Roy <robertproy@live.com>
2018-05-24 18:53:23 -0500

    improve commenting

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,9 +26,11 @@ $(function () {
 
     describe('The menu', function () {
         it('should not be visible by default', function () {
+            // menu-hidden class is responsible for hiding of the menu
             expect(document.body.classList.contains("menu-hidden")).toBe(true);
         });
         it('should be visible if clicked', function () {
+            // verifies clicking the menu-icon-link toggles the menu-hidden class properly
             $('.menu-icon-link').click();
             expect(document.body.classList.contains("menu-hidden")).toBe(false);
             $('.menu-icon-link').click();
@@ -38,9 +40,11 @@ $(function () {
     describe('Initial Entries', function () {
         beforeEach(function (done) {
             loadFeed(0);
+            // waits two seconds to allow api time to act
             setTimeout(done, 2000);
         })
         it('has entries present after loadFeed', function (done) {
+            // search for any articles with the length class inside of the .feed element
             var result = false;
             var $articles = $('.feed').find('article');
             for (var i = 0, len = $articles.length; i < len; i++) {
@@ -55,8 +59,10 @@ $(function () {
     });
 
     describe('New Feed Selection', function (done) {
-        var articlesOriginal = $('.feed').find('article');
-        var articlesNew = $('.feed').find('article');
+        // Load articles, save them under articlesOriginal. Then load a different feed,
+        // save it to articlesNew and compare the two. They should not match.
+        var articlesOriginal = '1';
+        var articlesNew = '2';
         beforeEach(function (done) {
             loadFeed(0);
             setTimeout(done, 2000);
@@ -70,10 +76,9 @@ $(function () {
         it('new feed actually changes the old feed', function (done) {
             articlesNew = $('.feed').find('article');
             expect(articlesNew).toBeDefined();
-            console.log(articlesOriginal);
-            console.log(articlesNew);
             expect(articlesNew === articlesOriginal).toBe(false);
             done();
         });
+        done();
     });
 });
